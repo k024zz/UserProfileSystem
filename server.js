61ab860dcba427c77782df1f7b55590e9606f022
k024zz <490874727@qq.com>
2016-03-31 23:31:04 -0400

    repair signup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var bcrypt = require("bcrypt-nodejs");
 var userData = require('./data.js');
 var Guid = require('Guid');
 
